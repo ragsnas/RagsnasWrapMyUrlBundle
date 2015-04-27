9aca7a9fd6af84936ab256cec9e1196ac1232453
ASchuermann <ragsnas@users.noreply.github.com>
2015-04-27 10:51:28 +0200

    Update overwrites.js
    Added ragsnasWrapMyUrlBundleLeaveUntouched Attribute to explicitly exclude certain urls from being wrapped.

--- Resources/public/js/overwrites.js
+++ Resources/public/js/overwrites.js
@@ -2,12 +2,20 @@
  * Created by aschuermann on 16.03.2015.
  */
 var ragsnasWrapMyUrlBundleRrewriteBaseUrl,
-    ragsnasWrapMyUrlBundleRewriteUrlEncode;
+    ragsnasWrapMyUrlBundleRewriteUrlEncode,
+    ragsnasWrapMyUrlBundleLeaveUntouched = [];
 
 (function() {
     if (typeof ragsnasWrapMyUrlBundleRrewriteBaseUrl !== 'undefined') {
         var proxied = window.XMLHttpRequest.prototype.open;
         window.XMLHttpRequest.prototype.open = function() {
+            if (ragsnasWrapMyUrlBundleLeaveUntouched.length > 0) {
+                for (var i = 0; i < ragsnasWrapMyUrlBundleLeaveUntouched.length ;i++) {
+                    if (arguments[1].substring(0, ragsnasWrapMyUrlBundleLeaveUntouched[i].length) === ragsnasWrapMyUrlBundleLeaveUntouched[i]) {
+                        return proxied.apply(this, [].slice.call(arguments));
+                    }
+                }
+            }
             var newUrl = ragsnasWrapMyUrlBundleRrewriteBaseUrl;
             arguments[1] = newUrl.replace('%s', (ragsnasWrapMyUrlBundleRewriteUrlEncode ? encodeURIComponent(arguments[1]) : arguments[1]));
 
